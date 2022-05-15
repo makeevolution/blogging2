0f5500b85feb6328bcb970ca895ad14b6f763b98
makeevolution <aldo_hasibuan@yahoo.com>
2022-05-15 21:56:21 +0200

    add form functionality to index.html

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ Notes
 ======
 - To run, start virtual env, then ```$env:FLASK_APP = "hello"``` and ```flask run```
 - add also ```$env:FLASK_ENV="development"```
-- 
+- This branch has a web form created using flask-wtf package
+- flask-wtf requires the app to have a secret key configured, to prevent against CSRF attacks
+- Each web form is a class, inherits from the class ```FlaskForm```
+- Each element in the web form is a field of the web form class, where the field
+  equals an object inherited from ```wtforms```
+- Use ```validators``` to validate the inputs. See hello.py for more examples
--- hello.py
+++ hello.py
@@ -1,15 +1,31 @@
+from wsgiref.validate import validator
 from flask import Flask, appcontext_popped, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config["SECRET_KEY"] = "hardtoguesstring"
+# validator datarequired() ensures the field is not empty
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("Add your email: ", validators=[Email()])
+    submit = SubmitField('Submit')
 
-@app.route('/')
+
+@app.route('/', methods=["GET","POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    return render_template("index.html",form=form,name=name, current_time=datetime.utcnow())
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html",name=name)
