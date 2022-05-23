fa34a204cd5a6a12cb98ae209900a1166a311eda
makeevolution <aldo_hasibuan@yahoo.com>
2022-05-23 14:34:54 +0200

    chapter 9: add unit tests to test user role and permissions

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,5 +1,5 @@
 from queue import Empty
-from app.models import User
+from app.models import AnonymousUser, Permission, User, Role 
 import unittest
 
 class UserModelTestCase(unittest.TestCase):
@@ -7,7 +7,7 @@ class UserModelTestCase(unittest.TestCase):
         # ARRANGE
         self.u = User()
         self.u.password = "12345"
-
+        
     def test_password_setter(self):
         # ACT & ASSERT
         self.assertTrue(self.u.password_hash is not None)
@@ -21,3 +21,33 @@ class UserModelTestCase(unittest.TestCase):
     def test_password_verifier(self):
         # ACT & ASSERT
         self.assertTrue(self.u.verify_password("12345"))
+    
+    def test_user_role(self):
+        user = User(role = Role.query.filter_by(name="User").first())
+        self.assertTrue(user.can(Permission.COMMENT))
+        self.assertTrue(user.can(Permission.FOLLOW))
+        self.assertTrue(user.can(Permission.WRITE))
+        self.assertFalse(user.can(Permission.MODERATE))
+        self.assertFalse(user.can(Permission.ADMIN))
+    def test_admin_role(self):
+        user = User(role = Role.query.filter_by(name="Administrator").first())
+        self.assertTrue(user.can(Permission.COMMENT))
+        self.assertTrue(user.can(Permission.FOLLOW))
+        self.assertTrue(user.can(Permission.WRITE))
+        self.assertTrue(user.can(Permission.MODERATE))
+        self.assertTrue(user.can(Permission.ADMIN))
+        self.assertTrue(user.is_administrator())
+
+    def test_moderator_role(self):
+        user = User(role = Role.query.filter_by(name="Moderator").first())
+        self.assertTrue(user.can(Permission.COMMENT))
+        self.assertTrue(user.can(Permission.FOLLOW))
+        self.assertTrue(user.can(Permission.WRITE))
+        self.assertTrue(user.can(Permission.MODERATE))
+        self.assertFalse(user.can(Permission.ADMIN))
+
+    def test_anonymous_user(self):
+        user = AnonymousUser()
+        for permission in filter(lambda x: not x.startswith('--'), dir(Permission)):
+            self.assertFalse(user.can(permission))
+
