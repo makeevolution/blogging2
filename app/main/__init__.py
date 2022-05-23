3bbc2229d7890a61413c017152177dcac719e2e1
makeevolution <aldo_hasibuan@yahoo.com>
2022-05-23 12:42:11 +0200

    add context processor to main blueprint, so every pages rendered by routes from main blueprint will have the variable injected by the processor, in this example it's Permission

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,7 +2,15 @@
 # Each type of route is defined in its own .py file
 
 from flask import Blueprint
+
+from app.models import Permission
 # The blueprint function below takes in the name of the blueprint, and the module where the blueprint is located (i.e. __name__, which means in this folder)
 main = Blueprint('main', __name__)
 from . import views, errors
 
+# The context processor below is added so that every render_template() call will make Permission available for jinja2.
+# Thus you don't have to do render_template(yourOtherArguments, Permission=Permission) everytime you want to render a page that
+# requires Permission
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
