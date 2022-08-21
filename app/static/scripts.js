bafe9eecc7c7e7cec5340221881cfba8493139a1
makeevolution <aldo_hasibuan@yahoo.com>
2022-08-21 09:21:42 +0200

    restyled navbar

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -11,4 +11,12 @@
             $('.show-post-form').toggle('show');
         });
     });
+    $(function() {
+        var navbar = document.getElementsByClassName("navbar")[0];
+        if (window.pageYOffset >= navbar.offsetTop) {
+          navbar.classList.add("sticky-navbar");
+        } else {
+          navbar.classList.remove("sticky-navbar");
+        }
+    });
 })(jQuery);
